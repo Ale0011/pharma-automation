344c50e152b2f67f05248a49ddd5f1c9e1511004
x3mns <36721347+Ale0011@users.noreply.github.com>
2025-11-13 14:37:05 -0400

    Add files via upload

---
+++ pharma-utils/index.js
@@ -0,0 +1,12 @@
+/**
+ * pharma-utils
+ * ------------
+ * General utility library used by the automation toolkit.
+ * Exposes simple helper functions (placeholder for expansion).
+ */
+
+function calc(a, b) {
+    return a + b;
+}
+
+module.exports = { calc };
